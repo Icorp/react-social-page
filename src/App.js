9436907f67ccbbdd7e02592d8f94a32cac34cdd7
Icorp <itteam1602@gmail.com>
2020-08-05 15:20:41 +0600

    Upload to Git

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className='header'>
+        <img src="https://logomaster.ai/static/media/gallery009.6a28a2e7.png"></img>
       </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div><a>Message</a></div>
+        <div><a>News</a></div>
+        <div><a>Music</a></div>
+        <div><a>Settings</a></div>
+      </nav>
+      <div className='content'>
+        <img src="https://image.freepik.com/free-vector/synthwave-retrowave_148087-120.jpg" />
+      </div>
     </div>
   );
 }
+
+
+
 
 export default App;
